431c1e5b04b1e50f2c9d8a1cff73ae5c7e102a5c
Arthur Mueller <AWM21@pitt.edu>
2019-04-10 11:18:22 -0400

    added login form

--- src/main/webapp/js/pothole.js
+++ src/main/webapp/js/pothole.js
@@ -4,6 +4,24 @@ let admin=false;
 
 function showLogin(){
     document.getElementById('login').innerHTML='Log In';
+    document.getElementById('login').addEventListener("click", function (showLoginForm){
+        let form=document.createElement("form");
+        form.setAttribute('method',"post");
+        form.setAttribute('action',"submit");
+        let username=document.createElement("input");
+        username.setAttribute('type',"text");
+        username.setAttribute('name',"username");
+        let password=document.createElement("input");
+        password.setAttribute('type',"text");
+        password.setAttribute('name',"password")
+        let submit=document.createElement("input");
+        submit.setAttribute('type',"submit");
+        submit.setAttribute('value',"submit");
+        form.appendChild(username);
+        form.appendChild(password);
+        form.appendChild(submit);
+        document.getElementById('main-navbar').appendChild(form);
+    });
 }
 
 function showSignUp(){
@@ -26,9 +44,8 @@ function showLogout(){
     document.getElementById('logout').innerHTML='Log Out';
 }
 
-
-
-if(noUser){showLogin();
+if(noUser){
+    showLogin();
     showSignUp();
 }
 
