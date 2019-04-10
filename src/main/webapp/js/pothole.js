6459abb92252a29afcd0ea737fbaf9683de92918
Arthur Mueller <AWM21@pitt.edu>
2019-04-10 12:51:12 -0400

    working on js

--- src/main/webapp/js/pothole.js
+++ src/main/webapp/js/pothole.js
@@ -21,8 +21,8 @@ function showLogin(){
         form.appendChild(password);
         form.appendChild(submit);
         document.getElementById('main-navbar').appendChild(form);
-    });
-}
+        document.getElementById('login').removeEventListener("click", function(showLoginForm){});
+})};
 
 function showSignUp(){
     document.getElementById('signup').innerHTML='Sign Up';
